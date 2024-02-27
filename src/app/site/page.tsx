8f50fbee533fe47c911b0a37f83b42af360a175f
T-N9 <thethoriumtension@gmail.com>
2024-02-27 10:05:45 +0630

    :lipstick: Hero section updated

--- src/app/site/page.tsx
+++ src/app/site/page.tsx
@@ -1,14 +1,27 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <section className="h-full w-full pt-36 relative flex items-center justify-center flex-col">
+    <section className="h-full w-full pt-36 relative flex items-center flex-col">
       <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]"><div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#C9EBFF,transparent)]"></div></div>
       <p className="text-center">Run your Agency, in one Place.</p>
+      <h1 className="text-center text-3xl -mb-2 md:text-6xl font-writing lg:-mb-12">Creator</h1>
       <div className="bg-gradient-to-r from-primary to-secondary-foreground text-transparent bg-clip-text relative">
         <h1 className="text-9xl font-bold text-center md:text-[300px]">
           Lark
         </h1>
       </div>
-      <h1 className="text-center text-6xl font-writing -mt-8">Creator</h1>
+
+      <div className="flex justify-center items-center relative md:mt-[-70px]">
+          <Image
+            src={'/assets/preview.png'}
+            alt="banner image"
+            height={1200}
+            width={1200}
+            className="rounded-tl-2xl rounded-tr-2xl border-2 border-muted"
+          />
+          <div className="bottom-0 top-[50%] bg-gradient-to-t dark:from-background left-0 right-0 absolute z-10"></div>
+        </div>
     </section>
   );
 }
