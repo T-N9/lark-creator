2f6bbba508ae782d64e3d42de1481c41d3281f44
T-N9 <thethoriumtension@gmail.com>
2024-02-27 14:06:33 +0630

    :lipstick: HomePage second section init

--- src/app/site/page.tsx
+++ src/app/site/page.tsx
@@ -2,20 +2,18 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <section className="h-full w-full pt-36 relative flex items-center flex-col">
-      <div className="absolute dark:hidden inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]"><div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#C9EBFF,transparent)]"></div></div>
-
-      <div className="absolute hidden dark:block top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]"></div>
-
-      <p className="text-center">Run your Agency, in one Place.</p>
-      <h1 className="text-center text-3xl -mb-2 md:text-6xl font-writing lg:-mb-12">Creator</h1>
-      <div className="bg-gradient-to-r from-primary to-secondary-foreground text-transparent bg-clip-text relative">
-        <h1 className="text-9xl font-bold text-center md:text-[300px]">
-          Lark
-        </h1>
-      </div>
-
-      <div className="flex justify-center items-center relative md:mt-[-70px]">
+    <>
+      <section className="h-full w-full pt-36 relative flex items-center flex-col">
+        <div className="absolute dark:hidden inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]"><div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#C9EBFF,transparent)]"></div></div>
+        <div className="absolute hidden dark:block top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]"></div>
+        <p className="text-center">Run your Agency, in one Place.</p>
+        <h1 className="text-center text-3xl -mb-2 md:text-6xl font-writing lg:-mb-12">Creator</h1>
+        <div className="bg-gradient-to-r from-primary to-secondary-foreground text-transparent bg-clip-text relative">
+          <h1 className="text-9xl font-bold text-center md:text-[300px]">
+            Lark
+          </h1>
+        </div>
+        <div className="flex justify-center items-center relative md:mt-[-70px]">
           <Image
             src={'/assets/preview.png'}
             alt="banner image"
@@ -25,6 +23,16 @@ export default function Home() {
           />
           <div className="bottom-0 top-[50%] bg-gradient-to-t dark:from-background left-0 right-0 absolute z-10"></div>
         </div>
-    </section>
+      </section>
+
+      <section className="flex justify-center items-center flex-col gap-4 md:!mt-20 mt-[-60px]">
+        <h2 className="text-4xl text-center"> Choose what fits you right</h2>
+        <p className="text-muted-foreground text-center">
+          Our straightforward pricing plans are tailored to meet your needs. If
+          {" you're"} not <br />
+          ready to commit you can get started for free.
+        </p>
+      </section>
+    </>
   );
 }
